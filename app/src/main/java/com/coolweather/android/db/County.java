f5a8c5e2879ac19c0608b2dca20eaf8190ac8e22
Zoom <2915955629@qq.com>
2022-03-14 10:29:17 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,45 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * 县表
+ */
+public class County extends LitePalSupport {
+    private int id;//县id
+    private String countyName;//县名
+    private String weatherId;//对应县天气
+    private int cityIdi;//所属城市id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityIdi() {
+        return cityIdi;
+    }
+
+    public void setCityIdi(int cityIdi) {
+        this.cityIdi = cityIdi;
+    }
+}
